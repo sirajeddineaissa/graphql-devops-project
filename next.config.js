48b8cbddee71525fd74dada3b9ecdab9b5724b57
sirajeddineaissa <sirajeddineaissa@protonmail.com>
2022-07-11 10:32:47 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  images: {
+    domains: [
+      "shopify.com",
+      "cdn.shopify.com"
+    ]
+  }
 }
-
-module.exports = nextConfig
