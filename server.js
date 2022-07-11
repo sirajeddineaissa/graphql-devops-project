0628632ca132d6d8b9067abc30a4887f244488b9
sirajeddineaissa <sirajeddineaissa@protonmail.com>
2022-07-11 12:36:38 +0100

    server-side script

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const {createServer} = require('http');
 const {parse} = require('url');
 const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV!=='production';
 const app = next({dev});
 const handle = app.getRequestHandler();
 const PORT = process.env.PORT || 3001;
