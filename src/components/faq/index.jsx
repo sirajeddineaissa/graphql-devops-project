eceaea2ee83b5360396a7e954b2369c62807988c
sirajeddineaissa <sirajeddineaissa@protonmail.com>
2022-07-20 17:56:14 +0100

    lender accordion data

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -1,12 +1,23 @@
 import Accordion from "@components/ui/accordion";
 import faqData from "@data/faq";
-
+import { Container, Row, Col } from "@bootstrap";
 const AccordionGroup = () => {
   return (
-    <section className="wrapper">
-      {faqData.map((faqItem) => (
-        <Accordion title={faqItem.title}>{faqItem.description}</Accordion>
-      ))}
+    <section>
+      <Container fluid className="mt-3 mt-md-5">
+        <Row>
+          <Col md={6}>
+            {faqData["borrower"].map((faqItem) => (
+              <Accordion title={faqItem.title}>{faqItem.description}</Accordion>
+            ))}
+          </Col>
+          <Col md={6}>
+            {faqData["lender"].map((faqItem) => (
+              <Accordion title={faqItem.title}>{faqItem.description}</Accordion>
+            ))}
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
